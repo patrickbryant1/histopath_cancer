900fb58bbb740f79b5d57be4a5ef34fd15adb101
patrick.bryant <patrick.bryant@gmail.com>
2019-03-03 21:05:10 +0100

    added comments

--- model.py
+++ model.py
@@ -13,6 +13,7 @@ from zipfile import ZipFile
 from PIL import Image
 from io import StringIO
 
+#Image information: all images are 96x96 color images with 3 channels (r,g,b)
 train_labels = pd.read_csv('/home/patrick/Documents/data/histopath_cancer/train_labels.csv')
 train_path = '/home/patrick/Documents/data/histopath_cancer/train.zip'
 test_path = '/home/patrick/Documents/data/histopath_cancer/test.zip'
@@ -39,5 +40,13 @@ train_zip = ZipFile(train_path, 'r')
 train_files = train_zip.infolist()
 
 #for i in range(0, len(train_files)):
-get_data(train_files[0].filename, train_zip)
+img = get_data(train_files[0].filename, train_zip)
 pdb.set_trace()
+
+
+#Split train data to use 90% for training and 10% for validation. 
+
+
+#A positive label is called if the center 32x32 pixels have at least one cancer polyp.
+#Cropping surely reduces the problem, but additional information from outside the crop zone is lost
+#All of this has to be taken into consideration. The crop size can thus be varied.

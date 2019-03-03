84d4e87e82dbb2aa3270ede1858f51370c2a78c7
patrick.bryant <patrick.bryant@gmail.com>
2019-03-03 20:15:48 +0100

    reads images into numpy arrays

--- model.py
+++ model.py
@@ -7,22 +7,37 @@ import pdb
 import numpy as np
 import pandas as pd
 import os
-import cv2
 import random
 from sklearn.utils import shuffle
+from zipfile import ZipFile
+from PIL import Image
+from io import StringIO
 
-data = pd.read_csv('/home/patrick/Documents/data/train_labels.csv')
-#train_path = '/kaggle/input/train/'
-#test_path = '/kaggle/input/test/'
+train_labels = pd.read_csv('/home/patrick/Documents/data/histopath_cancer/train_labels.csv')
+train_path = '/home/patrick/Documents/data/histopath_cancer/train.zip'
+test_path = '/home/patrick/Documents/data/histopath_cancer/test.zip'
 # quick look at the label stats
-#data['label'].value_counts()
+print(train_labels['label'].value_counts())
+
+
+def get_data(filename, archive):
+	'''A function for getting the .zip .tif images
+	into numpy arrays
+	'''
+	img = Image.open(archive.open(filename))
+	img_array = np.array(img)
+
+	return img_array
+
+
+#Test data
+test_zip = ZipFile(test_path, 'r')
+test_files = test_zip.infolist()
+
+#Train data
+train_zip = ZipFile(train_path, 'r')
+train_files = train_zip.infolist()
+
+#for i in range(0, len(train_files)):
+get_data(train_files[0].filename, train_zip)
 pdb.set_trace()
-
-def readImage(path):
-    # OpenCV reads the image in bgr format by default
-    bgr_img = cv2.imread(path)
-    # We flip it to rgb for visualization purposes
-    b,g,r = cv2.split(bgr_img)
-    rgb_img = cv2.merge([r,g,b])
-    return rgb_img
-

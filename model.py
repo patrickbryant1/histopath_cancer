6de4edfa189634c61ab927b951158c58ff6e9d19
patrick.bryant <patrick.bryant@gmail.com>
2019-03-04 11:30:19 +0100

    now reads data efficiently

--- model.py
+++ model.py
@@ -9,17 +9,21 @@ import pandas as pd
 import os
 import random
 from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 from zipfile import ZipFile
 from PIL import Image
 from io import StringIO
 
 #Image information: all images are 96x96 color images with 3 channels (r,g,b)
-train_labels = pd.read_csv('/home/patrick/Documents/data/histopath_cancer/train_labels.csv')
+data = pd.read_csv('/home/patrick/Documents/data/histopath_cancer/train_labels.csv')
 train_path = '/home/patrick/Documents/data/histopath_cancer/train.zip'
 test_path = '/home/patrick/Documents/data/histopath_cancer/test.zip'
-# quick look at the label stats
-print(train_labels['label'].value_counts())
 
+# quick look at the label stats
+def get_counts(data):
+	print(data['label'].value_counts())
+
+get_counts(data)
 
 def get_data(filename, archive):
 	'''A function for getting the .zip .tif images
@@ -37,14 +41,16 @@ test_files = test_zip.infolist()
 
 #Train data
 train_zip = ZipFile(train_path, 'r')
-train_files = train_zip.infolist()
+#train_files = train_zip.infolist()
 
-#for i in range(0, len(train_files)):
-img = get_data(train_files[0].filename, train_zip)
-pdb.set_trace()
-
+#Define names and labels
+train_df = data.set_index('id')
+train_names = train_df.index.values
+train_labels = np.asarray(train_df['label'].values)
 
 #Split train data to use 90% for training and 10% for validation. 
+tr_n, tr_idx, val_n, val_idx = train_test_split(train_names, range(len(train_names)), test_size=0.1, stratify=train_labels, random_state=123)
+img = get_data(tr_n[0]+'.tif', train_zip)
 
 
 #A positive label is called if the center 32x32 pixels have at least one cancer polyp.
